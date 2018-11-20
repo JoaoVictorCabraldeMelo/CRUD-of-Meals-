695382fafc9164a95e7d7cdfd183deff35097c59
JoaoVictorCabraldeMelo <joao.victor.cabralmelo@gmail.com>
2018-11-20 12:52:06 -0200

    Initial Commit

---
+++ db/migrate/20181113152649_create_meals.rb
@@ -0,0 +1,12 @@
+class CreateMeals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :meals do |t|
+      t.string :name
+      t.string :description
+      t.float :price
+      t.string :avaliable
+
+      t.timestamps
+    end
+  end
+end
